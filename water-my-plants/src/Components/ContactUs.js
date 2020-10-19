9519faabb758ff633cf77da28f377b8cf74c6fd8
Christopher Corvo <christophercorvo@gmail.com>
2020-10-19 18:16:05 -0400

    added schema's and event handlers

--- water-my-plants/src/Components/ContactUs.js
+++ water-my-plants/src/Components/ContactUs.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
+import * as yup from 'yup'
+import axios from 'axios'
 
-
+// --------------------- Basic Styling --------------------------------
 const ContactUsPage=styled.div`
     display:flex;
     justify-content: center;
@@ -11,23 +13,140 @@ const ContactUsForm = styled.form`
     flex-direction:column;
     background-color: #ECF1EB;
     width: 45%;
-
 `
+// --------------------- initial state values --------------------------
+
+const initialContactUsForm = {
+    name: '',
+    email: '',
+    message: '',
+}
+
+const initialContactUsErrors = {
+    name: '',
+    email: '',
+    message: '',
+}
+
+const initialDisabled = true
+
+const initialListOfContactForms = []
+
+// -------------------- ContactUs Schema -------------------------------
+
+
+const contactUsSchema = yup.object().shape({
+        name: yup
+            .string()
+            .required('Must include a name'),
+        email: yup
+            .string()
+            .email('Must be valid email address')
+            .required('Must include email address'),
+        message: yup
+            .string()
+            .required('Must include a message')
+
+})
 
 
 const ContactUs = () =>{
+    
+    // -------- slices of State ----------------------
+
+    const [listOfContactForms, setListOfContactForms] = useState(initialListOfContactForms)
+    const [contactUsForm, setcontactUsForm] = useState(initialContactUsForm) 
+    const [contactUsErrors, setContactUsErrors] = useState(initialContactUsErrors) 
+    const [disabled, setDisabled] = useState(initialDisabled)
+
+    // ---------------- Helper Functions ---------------------------
+    
+
+    // ---------------- Event Handlers -----------------------------
+    const onChange = evt => {
+        const {name, value} = evt.target
+        evt.preventDefault()
+        inputChange(name, value)
+    }
+    
+    const inputChange = (name, value) => {
+        yup
+            .reach(contactUsSchema, name)
+            .validate(value)
+            .then(valid => {
+                setContactUsErrors({
+                    ...contactUsErrors, [name]: ''})
+            })
+            .catch(err => {
+                setContactUsErrors ({
+                    ...contactUsErrors, [name]: err.errors[0]
+                })
+            })
+            
+            setcontactUsForm({
+                ...contactUsForm, [name]: value
+            })
+    }
+
+    const contactFormSubmit = () => {
+        
+        const newContactForm = {
+            name: contactUsForm.name.trim(),
+            email: contactUsForm.email.trim(),
+            message: contactUsForm.message.trim(),
+        }
+        postNewContactForm(newContactForm)
+    }
+    
+    const postNewContactForm = newContactForm => {
+        axios.post('', newContactForm) // need to add an api endpoint
+            .then(result => {
+                setListOfContactForms(listOfContactForms.concat(result.data))
+            })
+            .catch(err => {
+                console.log(err)
+            })
+            . finally(() => {
+                setcontactUsForm(initialContactUsForm)
+            })
+    }
+
+    useEffect(() => {
+        contactUsSchema.isValid(contactUsForm)
+        .then(valid => {
+            setDisabled(!valid);
+        })
+    }, [contactUsForm])
+    
     return(
         <ContactUsPage>
             
-            <ContactUsForm>
+            <ContactUsForm onsubmit={contactFormSubmit}>
                 <h1>Get in touch.</h1>
                 <label>Name:</label>
-                <input></input>
+                <input
+                    name = 'name'
+                    type = 'text'
+                    value = {contactUsForm.name}
+                    onChange= {onChange}
+                />
+
                 <label>Email</label>
-                <input></input>
+                <input
+                    name = 'email'
+                    type = 'email'
+                    value = {contactUsForm.email} 
+                    onChange = {onChange}/>
+
                 <label>Your message</label>
-                <input></input>
-                <button>Sign Up</button>
+                <input
+                    name = 'message'
+                    type = 'text'
+                    value = {contactUsForm.message}
+                    onChange = {onChange}/>
+
+                <button disabled={disabled}> Sign Up </button>
+
             </ContactUsForm>
         </ContactUsPage>
     )
--- water-my-plants/src/Components/SignIn.js
+++ water-my-plants/src/Components/SignIn.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
 import styled  from 'styled-components'
 import {Link} from 'react-router-dom'
+import axios from 'axios'
+
+import * as yup from 'yup'
 
 // -------------------- basic styles -------------------
 const SignInPage = styled.div`
@@ -16,33 +19,83 @@ const FormWrapper = styled.form`
 const QuoteSection = styled.div`
     background-color: #E1E6F4;
 `
-// -------------------------------------------------------
 
-const inititialSignIn = {
+// ----------------- initial state values --------------
+
+const inititialSignInForm = { //default signIn Form values
         username: '',
         password: '',
     }
 
+const initialSignInErrors ={ //default error form values
+    username: '',
+    password: '',
+}
 
+const initialDisabled = true // use this to make sure submit button only becomes active when forms are correctly filled
+
+// ---------------- SignIn schema --------------------------------------
+
+const signInSchema = yup.object().shape({
+    username: yup  
+        .string()
+        .required('Must include Username')
+        .min(5, 'Username needs 5 characters'),
+    
+    password: yup
+        .string()
+        .required('Must include Password')
+        .min(5, 'Password must have min 5 characters'),
+})
 
 
 const SignIn = () =>{
+        
+    // ------------- slices of state -----------------------
+    
+    const [signInForm, setSignInForm] = useState(inititialSignInForm)
+    const [signInErrors, setSignInErrors] = useState(initialSignInErrors)
+    const [disabled, setDisabled] = useState(initialDisabled)  
+
+    // -------------- helper functions ----------------------
+    
+   
+
+    // -------------- Event Handler -------------------------
+    const onChange = evt => {
+        const {name, value} = evt.target
+        evt.preventDefault()
+
+        inputChange(name, value)
+    }
+    
+    const inputChange = (name, value) => {
+        yup
+            .reach(signInSchema, name)
+            .validate(value)
+            .then(valid => {
+                setSignInErrors({
+                    ...signInErrors, [name]:''})
+            })
+            .catch(err => {
+                setSignInErrors({
+                    ...signInErrors,
+                    [name]: err.errors[0]
+                })
+            })
+
+            setSignInForm({
+                ...signInForm,
+                [name]: value,
+        })
+        
+    }
     
     
     
     
     
-    
-    // slices of state
-    const [signIn, setSignIn] = useState(inititialSignIn)
-    
-    
-    
-    
-    
-    
-    
-    // this is what renders to the page
+    // ------------ this is what renders to the page ---------------------
     
     return(
         <SignInPage>
@@ -52,20 +105,29 @@ const SignIn = () =>{
 
                 
                 <label>Username/Phone number</label>
-                <input></input>
-            
-
+                <input
+                    name = 'username'
+                    type = 'text'
+                    value = ''
+                    onChange = {onChange}
+                />
             
                 <label>Password</label>
-                <input></input>
+                <input
+                    name = 'username'
+                    type = 'text'
+                    value = ''
+                    onChange = {onChange}
+                />
+
                 <Link>
                     <p>Forgot password</p>
                 </Link>
-                  
                         
                 <Link>
-                    <button> Sign In </button>
+                    <button disabled = {disabled}> Sign In </button>
                 </Link>
+
             </FormWrapper>
 
             <QuoteSection>
