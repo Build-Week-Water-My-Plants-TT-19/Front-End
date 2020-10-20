608761440ef2c5d0e996f8f3f58d810cb8fde799
Christopher Corvo <christophercorvo@gmail.com>
2020-10-20 10:39:01 -0400

    test

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import './App.css';
 import {Route, Link} from 'react-router-dom'
 
@@ -8,31 +7,15 @@ import SignIn from './Components/SignIn';
 import ContactUs from './Components/ContactUs'
 import SignUp from './Components/SignUp'
 
-=======
-import SignUp from './Components/SignUp'
-import Plant from './Components/PlantForm'
-import './App.css';
-import PlantCard from './Components/PlantCard'
-import PlantForm from './Components/PlantForm'
->>>>>>> d29322657fb1a52878a7bb26c2cd6f3e2963c1ce
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
     
       <SignIn/>
       {/* <ContactUs/> */}
       
-=======
-      <SignUp/>
-      <br></br>
-      <Plant/>
-      <br></br>
-      <PlantCard/>
-      <br></br>
-      <PlantForm/>
->>>>>>> d29322657fb1a52878a7bb26c2cd6f3e2963c1ce
     </div>
   );
 }
