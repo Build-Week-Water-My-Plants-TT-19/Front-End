c411e49d6ea80d32ecf68baf03c767ab99c81cea
kayode <kayode.richards@gmail.com>
2020-10-19 20:58:05 -0400

    pulled christopher's code

--- water-my-plants/src/index.js
+++ water-my-plants/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
 
 import {BrowserRouter as Router} from 'react-router-dom';
 
@@ -12,15 +11,11 @@ ReactDOM.render(
       <App/>
     </Router>
   </React.StrictMode>,
-=======
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router } from 'react-router-dom'
-
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
->>>>>>> d29322657fb1a52878a7bb26c2cd6f3e2963c1ce
   document.getElementById('root')
 );
 
+// import SignUp from './Components/SignUp'
+// import Plant from './Components/PlantForm'
+// import './App.css';
+// import PlantCard from './Components/PlantCard'
+// import PlantForm from './Components/PlantForm'
