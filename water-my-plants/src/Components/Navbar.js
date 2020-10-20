e392491ee3da56b27304b5b0e7b6c2cf80a4f15e
GoldenPedro <pedropablo@casuso.us>
2020-10-20 10:59:09 -0500

    small updates

--- water-my-plants/src/Components/Navbar.js
+++ water-my-plants/src/Components/Navbar.js
@@ -4,9 +4,9 @@ import '../App.css';
 function Navbar() {
   return (
     <div className="navbar">
-      <div className='logo'>
+      <div className='logo-title'>
         <img src='https://notion-emojis.s3-us-west-2.amazonaws.com/v0/svg-twitter/1f340.svg' alt='plant logo' />
-        <p>WaterMyPlants</p>
+        <p><a href='/'>WaterMyPlants</a></p>
       </div>
 
       <div className='nav-links'>
